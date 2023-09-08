e43f1bbc773486a2b07016189453545ce1bc2129
Jsyrett977 <Jsyrett977@gmail.com>
2023-09-07 22:27:03 -0600

    login inputs and state

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,19 @@
-const Home = () => {
+import { useState } from "react";
 
+const Home = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    return (
+        <>
+        <h1>
+            Parry's Track
+        </h1>
+        <form id='loginForm'>
+            
+            <input className='loginInput' placeholder="Email" type="text" value={email} onChange={event => setEmail(event.target.value)}></input>
+            <input className="loginInput" placeholder="Password" type="text" value={password} onChange={event => setPassword(event.target.value)}></input>
+        </form>
+        </>
+    )
 }
 export default Home;
